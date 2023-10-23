4bfbc753950638da540cb6633f1321a2593912dd
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-10-23 15:14:43 +0200

    Route setter and getter

--- src/main/java/buildWeek/entities/Route.java
+++ src/main/java/buildWeek/entities/Route.java
@@ -18,4 +18,71 @@ public class Route {
     private Transport meansOfTransport;
    @ManyToOne
    private Travel travel;
+
+    public Route() {}
+
+    public Route(String startRoutePlace, String endRoutePlace, int duration, Transport meansOfTransport, Travel travel) {
+        this.startRoutePlace = startRoutePlace;
+        this.endRoutePlace = endRoutePlace;
+        this.duration = duration;
+        this.meansOfTransport = meansOfTransport;
+        this.travel = travel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getStartRoutePlace() {
+        return startRoutePlace;
+    }
+
+    public void setStartRoutePlace(String startRoutePlace) {
+        this.startRoutePlace = startRoutePlace;
+    }
+
+    public String getEndRoutePlace() {
+        return endRoutePlace;
+    }
+
+    public void setEndRoutePlace(String endRoutePlace) {
+        this.endRoutePlace = endRoutePlace;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Transport getMeansOfTransport() {
+        return meansOfTransport;
+    }
+
+    public void setMeansOfTransport(Transport meansOfTransport) {
+        this.meansOfTransport = meansOfTransport;
+    }
+
+    public Travel getTravel() {
+        return travel;
+    }
+
+    public void setTravel(Travel travel) {
+        this.travel = travel;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "id=" + id +
+                ", startRoutePlace='" + startRoutePlace + '\'' +
+                ", endRoutePlace='" + endRoutePlace + '\'' +
+                ", duration=" + duration +
+                ", meansOfTransport=" + meansOfTransport +
+                ", travel=" + travel +
+                '}';
+    }
 }
--- src/main/java/buildWeek/entities/Travel.java
+++ src/main/java/buildWeek/entities/Travel.java
@@ -74,4 +74,5 @@ public class Travel {
                 ", transport=" + transport +
                 '}';
     }
+    
 }
