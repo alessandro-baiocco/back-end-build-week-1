8d761d3414a15ade898309c52b0b55a1f30e2006
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-10-23 23:01:17 +0200

    Inseriti dati nel database

--- src/main/java/buildWeek/Application.java
+++ src/main/java/buildWeek/Application.java
@@ -1,7 +1,10 @@
 package buildWeek;
 
 import buildWeek.dao.*;
-import buildWeek.entities.UserBadge;
+import buildWeek.entities.*;
+import buildWeek.enums.SellerType;
+import buildWeek.enums.TicketDuration;
+import buildWeek.enums.TransportType;
 import net.datafaker.Faker;
 
 import javax.persistence.EntityManager;
@@ -10,6 +13,8 @@ import javax.persistence.Persistence;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
 
@@ -41,66 +46,86 @@ public class Application {
         };
 
 
-//        Supplier<Seller> SellerSupplier = () -> {
-//            return new Seller(true , SellerType.AUTO);
-//        };
+       Supplier<Seller> SellerSupplier = () -> {
+            return new Seller(true , SellerType.AUTO);
+       };
+
+      /*  for (int d = 0; d < 10; d++) {
+           userDAO.save(userSupplier.get());
+        }
 
 
-//        for (int i = 0; i < 10; i++) {
-//            if (i % 2 == 0) {
-//                sellDao.save(new Seller(true, SellerType.AUTO));
-//
-//            } else {
-//                sellDao.save(new Seller(false, SellerType.REAL));
-//            }
 
-//        List<TicketDuration> lista = new ArrayList<>();
-//        lista.add(TicketDuration.WEEKLY);
-//        lista.add(TicketDuration.MONTHLY);
+       for (int i = 0; i < 10; i++) {
+           if (i % 2 == 0) {
+                sellDao.save(new Seller(true, SellerType.AUTO));
 
+            } else {
+                sellDao.save(new Seller(false, SellerType.REAL));
+            }
 
-//        for (int i = 0; i < 10; i++) {
-//
-//            subDao.save(new Subscription(now.minusMonths(rnd.nextInt(5, 10)),
-//                    sellDao.getRandomSeller(),
-//                    lista.get(rnd.nextInt(0, 2)), now.minusMonths(rnd.nextInt(2, 5))
-//                    , userDAO.getRandomUserBadge()));
-//
-//
-//        }
-
-//        for (int i = 0; i < 5; i++) {
-//            transDao.save(new Transport(faker.name().title(), TransportType.TRAM, 80, true));
-//        }
-
-//        for (int i = 0; i < 10; i++) {
-//            servDao.save(new Service(transDao.getById(rnd.nextInt(96, 115)), now.minusDays(rnd.nextInt(20, 30))));
-//        }
+       List<TicketDuration> lista = new ArrayList<>();
+        lista.add(TicketDuration.WEEKLY);
+        lista.add(TicketDuration.MONTHLY);
 
 
-//        for (int i = 0; i < 10; i++) {
-//            rouDao.save(new Route(faker.futurama().location(), faker.futurama().location(), rnd.nextInt(20, 40)));
-//        }
+        for (int a = 0; a < 10; a++) {
 
-//        for (int i = 0; i < 10; i++) {
-//            LocalDateTime coso = now2.minusDays(rnd.nextInt(20, 30));
-//            travDao.save(new Travel(coso,
-//                    coso.plus(randomMinutes, ChronoUnit.valueOf("MINUTES")),
-//                    rouDao.findById(rnd.nextInt(126, 135)),
-//                    transDao.getById(rnd.nextInt(96, 115))));
-//        }
+            subDao.save(new Subscription(now.minusMonths(rnd.nextInt(5, 10)),
+                    sellDao.getRandomSeller(),
+                   lista.get(rnd.nextInt(0, 2)), now.minusMonths(rnd.nextInt(2, 5))
+                    , userDAO.getRandomUserBadge()));
 
-//        for (int i = 0; i < 10; i++) {
-//            rouDao.setTrans();
-//        rouDao.setTravel();
-//        }
+
+        }
+
+ for (int e = 0; e < 5; e++) {
+            transDao.save(new Transport(faker.name().title(), TransportType.BUS, 80, true));
+        }
+
+        for (int o = 0; o < 10; o++) {
+            servDao.save(new Service(transDao.getById(rnd.nextInt(96, 115)), now.minusDays(rnd.nextInt(20, 30))));
+        }
+
+
+        for (int u = 0; u < 10; u++) {
+            rouDao.save(new Route(faker.futurama().location(), faker.futurama().location(), rnd.nextInt(20, 40)));
+        }
+  for (int u = 0; u < 10; u++) {
+            rouDao.save(new Route(faker.futurama().location(), faker.futurama().location(), rnd.nextInt(20, 40)));
+        }
+for (int e = 0; e < 5; e++) {
+            transDao.save(new Transport(faker.name().title(), TransportType.TRAM, 80, true));
+        }
+
+        }
+
+for (int c = 0; c < 10; c++) {
+            LocalDateTime coso = now2.minusDays(rnd.nextInt(20, 30));
+            travDao.save(new Travel(coso,
+                    coso.plus(randomMinutes, ChronoUnit.valueOf("MINUTES")),
+                    rouDao.findById(rnd.nextInt(72, 82)),
+                    transDao.getById(rnd.nextInt(57, 102))));
+        }
+
+
+ for (int w = 0; w < 10; w++) {
+            rouDao.setTrans();
+            rouDao.setTravel();
+        }
+        */
+
+
+
 
         em.close();
         emf.close();
-    }
+           }
+
+       }
 
 
-}
+
 
 
 
--- src/main/java/buildWeek/dao/RouteDAO.java
+++ src/main/java/buildWeek/dao/RouteDAO.java
@@ -72,7 +72,9 @@ public class RouteDAO {
         transaction.begin();
         TypedQuery<Route> getAllRoute = em.createQuery("SELECT t FROM Route t", Route.class); // Query JPQL
         List<Route> boh = getAllRoute.getResultList();
-        boh.forEach(t -> t.setMeansOfTransport(getRandomTrans()));
+        boh.forEach(t ->
+            t.setMeansOfTransport(getRandomTrans()) );
+
         // 2. Eseguo la query
         // 3. Termino la transazione col salvataggio effettivo di una nuova riga nella tabella students
         transaction.commit();
