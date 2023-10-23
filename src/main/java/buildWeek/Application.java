e62f7cf25390f9c6e25ea4c17f5c0b36cfe85b63
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-23 22:19:48 +0200

    riempimento database

--- src/main/java/buildWeek/Application.java
+++ src/main/java/buildWeek/Application.java
@@ -1,19 +1,15 @@
 package buildWeek;
 
-import buildWeek.dao.SellerDao;
-import buildWeek.dao.SubscriptionDAO;
-import buildWeek.dao.UserBadgeDao;
-import buildWeek.entities.Subscription;
+import buildWeek.dao.*;
 import buildWeek.entities.UserBadge;
-import buildWeek.enums.TicketDuration;
 import net.datafaker.Faker;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Random;
 import java.util.function.Supplier;
 
@@ -25,12 +21,20 @@ public class Application {
         EntityManager em = emf.createEntityManager();
         UserBadgeDao userDAO = new UserBadgeDao(em);
         SellerDao sellDao = new SellerDao(em);
+        TransportDao transDao = new TransportDao(em);
         SubscriptionDAO subDao = new SubscriptionDAO(em);
+        TravelDao travDao = new TravelDao(em);
+        ServiceDao servDao = new ServiceDao(em);
+        RouteDAO rouDao = new RouteDAO(em);
+
         Faker faker = new Faker();
         Random rnd = new Random();
 
 
         LocalDate now = LocalDate.now();
+        LocalDateTime now2 = LocalDateTime.now();
+        int randomMinutes = 20 + rnd.nextInt(21);
+        LocalDateTime randomDateTime = now2.plus(randomMinutes, ChronoUnit.MINUTES);
 
         Supplier<UserBadge> userSupplier = () -> {
             return new UserBadge(faker.name().firstName(), faker.name().lastName(), now.minusYears(rnd.nextInt(20, 50)), now.minusYears(rnd.nextInt(2, 5)));
@@ -50,21 +54,46 @@ public class Application {
 //                sellDao.save(new Seller(false, SellerType.REAL));
 //            }
 
-        List<TicketDuration> lista = new ArrayList<>();
-        lista.add(TicketDuration.WEEKLY);
-        lista.add(TicketDuration.MONTHLY);
+//        List<TicketDuration> lista = new ArrayList<>();
+//        lista.add(TicketDuration.WEEKLY);
+//        lista.add(TicketDuration.MONTHLY);
 
 
-        for (int i = 0; i < 10; i++) {
+//        for (int i = 0; i < 10; i++) {
+//
+//            subDao.save(new Subscription(now.minusMonths(rnd.nextInt(5, 10)),
+//                    sellDao.getRandomSeller(),
+//                    lista.get(rnd.nextInt(0, 2)), now.minusMonths(rnd.nextInt(2, 5))
+//                    , userDAO.getRandomUserBadge()));
+//
+//
+//        }
 
-            subDao.save(new Subscription(now.minusMonths(rnd.nextInt(5, 10)),
-                    sellDao.getRandomSeller(),
-                    lista.get(rnd.nextInt(0, 2)), now.minusMonths(rnd.nextInt(2, 5))
-                    , userDAO.getRandomUserBadge()));
+//        for (int i = 0; i < 5; i++) {
+//            transDao.save(new Transport(faker.name().title(), TransportType.TRAM, 80, true));
+//        }
+
+//        for (int i = 0; i < 10; i++) {
+//            servDao.save(new Service(transDao.getById(rnd.nextInt(96, 115)), now.minusDays(rnd.nextInt(20, 30))));
+//        }
 
 
-        }
+//        for (int i = 0; i < 10; i++) {
+//            rouDao.save(new Route(faker.futurama().location(), faker.futurama().location(), rnd.nextInt(20, 40)));
+//        }
 
+//        for (int i = 0; i < 10; i++) {
+//            LocalDateTime coso = now2.minusDays(rnd.nextInt(20, 30));
+//            travDao.save(new Travel(coso,
+//                    coso.plus(randomMinutes, ChronoUnit.valueOf("MINUTES")),
+//                    rouDao.findById(rnd.nextInt(126, 135)),
+//                    transDao.getById(rnd.nextInt(96, 115))));
+//        }
+
+//        for (int i = 0; i < 10; i++) {
+//            rouDao.setTrans();
+//        rouDao.setTravel();
+//        }
 
         em.close();
         emf.close();
