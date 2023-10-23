6879ecf45c520d94db6914132c5fa62e16e56118
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-23 16:02:35 +0200

    enums folder fix

--- src/main/java/buildWeek/enums/TicketDuration.java
+++ src/main/java/buildWeek/enums/TicketDuration.java
@@ -1,12 +1,4 @@
-<<<<<<<< HEAD:src/main/java/buildWeek/enums/TicketDuration.java
 package buildWeek.enums;
-========
-<<<<<<<< Updated upstream:src/main/java/Enums/TicketDuration.java
-package Enums;
-========
-package buildWeek.enums;
->>>>>>>> Stashed changes:src/main/java/buildWeek/enums/TicketDuration.java
->>>>>>>> travels-and-route:src/main/java/Enums/TicketDuration.java
 
 public enum TicketDuration {
     WEEKLY,
